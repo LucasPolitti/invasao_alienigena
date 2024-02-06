0204fbfad40f9fc6cce7722dd19a675b9106c615
Lucas Politti <lucaspolitti7@gmail.com>
2024-02-06 09:41:18 -0300

    Restringindo o alcance da espaçonave

--- configuracoes.py
+++ configuracoes.py
@@ -7,3 +7,4 @@ class Configuracoes:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (100, 100, 230)
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -7,13 +7,17 @@ class Ship:
         """Inicializa a espaçonave e difine sua posição inicial"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.configuracoes = ai_game.configuracoes
 
         #Sobe a imagem da espaçonave e obtém seu rect
         self.image = pygame.image.load('imagens/ship.bmp')
         self.rect = self.image.get_rect()
 
-        #Começa cada espaçonave nova no centro inferior da tela
+        #Coloca cada espaçonave nova no centro inferior da tela
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Armazena um float para a posição horizontal exata da espaçonave
+        self.x = float(self.rect.x)
 
         #Flags de movimento; começa com uma espaçonave que não está se movendo
         self.moving_right = False
@@ -21,10 +25,14 @@ class Ship:
 
     def update(self):
         """Atualiza a posição da espaçonave com base na flag de movimento"""
-        if self.moving_right:
-            self.rect.x += 4
-        if self.moving_left:
-            self.rect.x -= 4
+        # Atualiza o valor x da espaçonave, não o rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.configuracoes.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.configuracoes.ship_speed
+
+        #Atualiza o objeto rect de self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Desenha a espaçonave em sua localização atual"""
